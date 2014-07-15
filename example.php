9dc4dfd57eb11879392a5d1c715f1da53257a9d7
Onur BALCI <onurbalc@gmail.com>
2014-07-15 17:09:58 +0300

    Indenttation changed Tabs to Spaces

--- example.php
+++ example.php
@@ -3,9 +3,9 @@
 require_once "Deploy.php";
 
 $deploy = new Deploy(
-	'422ebdfcfff3c36ac146301cd6094bf3',
-	'git@github.com:legoritma/PHP-git-deploy.git',
-	dirname(__FILE__). '/deploy/'
+    '422ebdfcfff3c36ac146301cd6094bf3',
+    'git@github.com:legoritma/PHP-git-deploy.git',
+    dirname(__FILE__). '/deploy/'
 );
 
 $deploy->start($_GET['token']);
